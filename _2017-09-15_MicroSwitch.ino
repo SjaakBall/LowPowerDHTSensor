65c62ab3305d15de30cd179e3def938c16c8e923
Jan Herman van der Sloot <SjaakBall@users.noreply.github.com>
2017-09-15 14:43:45 +0200

    Create _2017-09-15_MicroSwitch.ino

---
+++ _2017-09-15_MicroSwitch.ino
@@ -0,0 +1,121 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+ESP8266WiFiMulti WiFiMulti;
+ADC_MODE(ADC_VCC);
+
+#define VCC_ADJ   1.096
+
+#define pushButton    2
+
+const char* ssid     = "FartNet";
+const char* password = "1214406964";
+const char* host = "192.168.178.139";
+const uint16_t port = 80;
+
+// for mac address
+uint8_t MAC_array[6];
+char MAC_char[18];
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  WiFiMulti.addAP(ssid, password);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Wait for WiFi");
+
+  byte maxwait = 20;
+
+  while ((WiFiMulti.run() != WL_CONNECTED) && maxwait > 0) {
+    Serial.print(".");
+    delay(500);
+    maxwait--;
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  delay(500);
+
+  pinMode(pushButton, INPUT);
+
+  getMacAddress();
+}
+
+void loop() {
+  Serial.println("Begin loop \n\n");
+  if (WiFiMulti.run() == WL_CONNECTED) {
+
+    String batteryLevel = readBatteryLevel();
+
+    int buttonState = digitalRead(pushButton);
+    String trapStatus = "false";
+    if (buttonState == 1) {
+      trapStatus = "true";
+    } else {
+      trapStatus = "false";
+    }
+    Serial.println(trapStatus);
+
+    String url = "/mousetrap/temp/" + String(MAC_char) + "/" + String((int)buttonState) + "/" + batteryLevel;
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+
+    sendHttpData(url);
+  }
+  delay(10000);
+  //ESP.deepSleep(100000);
+  //Serial.println("Start deep sleep");
+  //ESP.deepSleep(120000000, WAKE_RF_DEFAULT);
+  //Serial.println("\n\nEnd loop");
+}
+
+void sendHttpData(String url) {
+  Serial.print("connecting to ");
+  Serial.println(host);
+  Serial.println(port);
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    Serial.println("wait 5 sec...");
+    delay(5000);
+    return;
+  }
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  delay(500);
+
+  // Read all the lines of the reply from server and print them to Serial
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+}
+
+String readBatteryLevel() {
+  String batteryLevel = String((ESP.getVcc() * VCC_ADJ) / 1000);
+  //webservice cannot cope with . so replace with dec
+  batteryLevel.replace(".", "dec");
+  return batteryLevel;
+}
+
+void getMacAddress() {
+  WiFi.macAddress(MAC_array);
+  for (int i = 0; i < sizeof(MAC_array); ++i) {
+    sprintf(MAC_char, "%s%02x:", MAC_char, MAC_array[i]);
+  }
+}
